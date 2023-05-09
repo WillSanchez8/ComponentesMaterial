80a8eb93ecb7efa035244f97a033265f90d62ccc
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-05-08 21:27:33 -0600

    creación de componentes list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -85,6 +85,9 @@ import { InputComponent } from './components/input/input.component';
 import { Input1Component } from './components/input/input1/input1.component';
 import { ListComponent } from './components/list/list.component';
 import { List1Component } from './components/list/list1/list1.component';
+import { List2Component } from './components/list/list2/list2.component';
+import { List3Component } from './components/list/list3/list3.component';
+import { List4Component } from './components/list/list4/list4.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -148,7 +151,10 @@ import { List1Component } from './components/list/list1/list1.component';
     InputComponent,
     Input1Component,
     ListComponent,
-    List1Component
+    List1Component,
+    List2Component,
+    List3Component,
+    List4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/list/list2/list2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list2',
+  templateUrl: './list2.component.html',
+  styleUrls: ['./list2.component.css']
+})
+export class List2Component {
+
+}
